d6722dbfdafec373e4f7e120980b205f509d97b6
9JoyBoy6 <63367431+9JoyBoy6@users.noreply.github.com>
2020-04-10 16:44:26 +0200

    Create enterInTheLoop.cpp

---
+++ enterInTheLoop.cpp
@@ -0,0 +1,13 @@
+
+
+double lastTime = getSystemTime(); 
+
+while (!gameOver) 
+{ 
+  double currentTime = getSystemTime (); 
+  double deltaTime = currentTime - lastTime; 
+  CheckInput(); 
+  Update(deltaTime); 
+  Draw(); 
+  lastTime = currentTime; 
+}
