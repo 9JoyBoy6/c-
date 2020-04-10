87229850bbcb66ccd0c0015eea19315919614937
9JoyBoy6 <63367431+9JoyBoy6@users.noreply.github.com>
2020-04-10 16:47:05 +0200

    Update enterInTheLoop.cpp

--- enterInTheLoop.cpp
+++ enterInTheLoop.cpp
@@ -4,7 +4,7 @@ double lastTime = getSystemTime();
 
 while (!gameOver) 
 { 
-  double currentTime = getSystemTime (); 
+  double currentTime = getSystemTime(); 
   double deltaTime = currentTime - lastTime; 
   CheckInput(); 
   Update(deltaTime); 
